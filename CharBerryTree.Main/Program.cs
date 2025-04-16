17a1664fdd4584bcaaf80d2c1abf7af1db9411e7
StrawmanDave <davidgb.strawman@gmail.com>
2025-04-15 20:11:22 -0700

    CharBerryTree complete

---
+++ CharBerryTree.Main/Program.cs
@@ -0,0 +1,9 @@
+﻿using CharBerryTree;
+CharberryTree tree = new CharberryTree();
+
+Notifier announcer = new Notifier(tree);
+Harvester harvester = new Harvester(tree);
+while(true)
+{
+    tree.MaybeGrow();
+}
---
+++ CharBerryTree/Class1.cs
@@ -0,0 +1,52 @@
+﻿namespace CharBerryTree;
+
+public class Class1
+{
+
+}
+
+public class CharberryTree
+{
+    private Random _random = new Random();
+    public bool Ripe {get; set;}
+    public event Action? Ripened;
+
+    public void MaybeGrow()
+    {
+        // Only a tiny chance of ripening each time, but we try a lot!
+        if (_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripened?.Invoke();
+        }
+    }
+}
+
+public class Notifier
+{
+    public Notifier(CharberryTree tree)
+    {
+        tree.Ripened += OnTreeRipened;
+    }
+
+    private void OnTreeRipened() => Console.WriteLine("The tree is ripe.");
+}
+
+public class Harvester
+{
+    private int _harvestCount;
+    private CharberryTree _tree;
+
+    public Harvester(CharberryTree tree)
+    {
+        _tree = tree;
+        _tree.Ripened += OnTreeRipened;
+    }
+
+    private void OnTreeRipened()
+    {
+        _harvestCount++;
+        _tree.Ripe = false;
+        Console.WriteLine($"The tree has been harvested {_harvestCount} times.");
+    }
+}
